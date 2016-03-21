b0590f079b12636e5366569a10e1ab4bb4b31bfb
Druvan <edvin.lundberg@gmail.com>
2016-03-21 11:51:06 +0100

    Addition,Division nästan klar

--- src/symbolic/JunitTest.java
+++ src/symbolic/JunitTest.java
@@ -10,12 +10,23 @@ public class JunitTest {
         @org.junit.Test
         public void main() throws Exception {
 
+            //Addition.java
+            AdditionTest testAdd = new AdditionTest();
+            testAdd.getName();
+            testAdd.eval();
+            testAdd.getResult();
+
             //Constant.java
             ConstantTest testConst = new ConstantTest();
             testConst.getName();
             testConst.getConstant();
 
-            //
+            //Division.java
+            DivisionTest testDiv = new DivisionTest();
+            testDiv.getName();
+            testDiv.eval();
+            testDiv.getResult();
+
         }
 
 
